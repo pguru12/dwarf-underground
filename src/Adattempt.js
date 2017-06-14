c25980062237af5644d9b3f7b2062313ba1d1558
Pranav <pguru12@gmail.com>
2017-06-14 11:32:20 -0400

    tried to simplify ads

---
+++ src/Adattempt.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Adattempt extends Component(string, string, string){
+    render(){
+        return (
+        <div className="small-6 medium-3 columns other-article">
+          <a href="#">
+            <img src="string" alt="string" />
+            <p>string</p>
+          </a> 
+        </div>
+        )
+    }
+}
+export default Adattempt
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ParOne from './ParOne'
 import ParTwo from './ParTwo'
 import Links from './Links'
 import Hammer from './Hammer'
+//import Adattempt from './Adattempt'
 import SingleSheet from './SingleSheet'
 
 class App extends Component {
@@ -24,7 +25,11 @@ class App extends Component {
           <Hammer/>
       <div className="small-12 columns other-articles">
          <h2>From around the Realm</h2>
-          <SingleSheet/>
+            <SingleSheet/>
+            {/*Adattempt(https://www.enclavepublishing.com/wp-content/uploads/2015/11/tumblr_inline_mtvwr6T4qH1r8eb4v.gif, orc, Single Orcs in Indianapolis)
+            Adattempt(https://images.fineartamerica.com/images-medium-large-5/rocky-mountain-landscape-meadow-in-spring-western-panorama-with-wildflowers-square-format-walt-curlee.jpg, mountain, You won't believe what's under this mountain)
+            Adattempt(http://orig01.deviantart.net/a278/f/2010/357/9/7/seamless___gold_coins_by_bartalon-d35iydr.jpg, gold, Mine 20% more gold with One Weird Trick)
+            Adattempt(http://cdn23.us1.fansshare.com/photos/thehobbit/the-hobbit-square-characters-833282558.jpg, hobbit, Surprise for Indiana Hobbits born before 1999)            */}
       </div>
     </main>
 
